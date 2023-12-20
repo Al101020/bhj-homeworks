1c62a1307484f57512e029d2307ff4dbea002178
alexgithub <alexgithub@yandex.ru>
2023-12-20 14:08:42 +1000

    second commit async-requests

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,10 +1,16 @@
-const progress = document.getElementById( 'progress' );
-// progress.value = 0.7; // работает(изменения видно)
+document.getElementById('form').addEventListener('submit', (el) => {
+    el.preventDefault()
 
-const xhr = new XMLHttpRequest(); // Создаём новый объект XMLHttpRequest
-xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/upload') // создаем запрос с двумя аргументами method и url. При отсутствии аргумента async метод open создаёт запрос асинхронным по умолчанию
-xhr.send() // запрос отправлен
+    const xhr = new XMLHttpRequest()
 
-xhr.addEventListener('readystatechange', () => { // Нужно подписаться на событие - на КАКОЕ?
-    progress.value = xhr.upload;
-});
+    xhr.upload.onprogress = function(event) {
+        let aa = event.loaded;
+        let bb = event.total;
+        const progress = document.getElementById( 'progress' );
+        progress.value = aa / bb;
+    };
+    
+    xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
+    const formData = new FormData(document.getElementById('form'));
+    xhr.send(formData);
+});  
